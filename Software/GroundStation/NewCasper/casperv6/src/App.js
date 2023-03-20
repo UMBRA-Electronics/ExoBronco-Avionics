bc9a15ebe37dbc185c7c0103eae4730977861f95
Daniel Pasion <dnpaxion@gmail.com>
2023-03-19 23:47:35 -0700

    commiting final changes of spinners, will now implement rocketship states and percentages

--- Software/GroundStation/NewCasper/casperv6/src/App.js
+++ Software/GroundStation/NewCasper/casperv6/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Casper V6</h1>
+      <div class="first-row">
+        <div id="title">Casper V6</div>
+          <div class="timer">
+            <div id="timer-title">Mission Elapsed Timer</div>
+            <div id="clock">00:00:00</div>
+          </div>
+      </div>
       <div className="second-row">
         <img src="https://www.freeiconspng.com/thumbs/rocket-ship-png/rocket-ship-png-7.png" alt="Rocket Ship" id = "rocketshipimage"></img>
         <div className="important-info-box">
@@ -14,15 +20,37 @@ function App() {
               <div className ="circle">4</div>
               <div className ="circle">5</div>
           </div>
-          <div className = "kinematic-container">
+          <div className = "pie-container">
             <div className="velocity-container">
-              <div id="velocity">69.420m/s</div>
-              <div id="velocity-title">Total Rocket Velocity</div>
+              <div className="pie" id="velocity">42.0%</div>
+              <div className="pie-title">Total Rocket Velocity</div>
             </div>
             <div className="acceleration-container">
-              <div className="pie" id="acceleration">69%</div>
-              <div id="acceleration-title">Acceleration</div>
+              <div className="pie" id="acceleration">13%</div>
+              <div className="pie-title">Acceleration</div>
             </div>
+            <div className="pitch-container">
+              <div className="pie" id="pitch">12%</div>
+              <div className="pie-title">Pitch</div>
+            </div>
+            <div className="yaw-container">
+              <div className="pie" id="yaw">99%</div>
+              <div className="pie-title">Yaw</div>
+            </div>
+            <div className="gyro-container">
+              <div className="pie" id="gyro">80%</div>
+              <div className="pie-title">Gyro</div>
+            </div>
+            <div className="roll-container">
+              <div className="pie" id="roll">10%</div>
+              <div className="pie-title">Roll</div>
+            </div>
+          </div>
+        </div>
+        <div className="line-values">
+          <div className="RSSI-container">
+            <div id="RSSI">RSSI</div>
+            <div className="line"></div>
           </div>
         </div>
       </div>
